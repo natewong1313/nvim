89b59214b7d9feffaa14c93db9e5aadc7b9aac2e
Nate Wong <natewong1@gmail.com>
2024-12-18 12:46:41 -0500

    setup ts and astro

--- init.lua
+++ init.lua
@@ -626,7 +626,8 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
+        ts_ls = {},
+        astro = {},
         --
 
         lua_ls = {
@@ -900,7 +901,24 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'go' },
+      ensure_installed = {
+        'bash',
+        'c',
+        'diff',
+        'html',
+        'lua',
+        'luadoc',
+        'markdown',
+        'markdown_inline',
+        'query',
+        'vim',
+        'vimdoc',
+        'go',
+        'css',
+        'typescript',
+        'javascript',
+        'astro',
+      },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
